468bd00e183640fffe22ab7e41ed436e1b611564
Joseph V <joseph.vonedwins@yahoo.com>
2018-10-07 14:37:53 -0400

    small styling updates

--- client/src/components/Search/Search.js
+++ client/src/components/Search/Search.js
@@ -6,7 +6,7 @@ class Search extends React.Component {
     render() {
         return (
             <div className="search-box">
-            <h2>Article Search</h2>
+            <h2 className="search-title">Article Search</h2>
                 <label>Topic</label>
                 <br></br>
                 <input
