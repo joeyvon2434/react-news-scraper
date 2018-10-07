5b6dd5a7a2d0d0fb43106ab0c8a1a800d6a18a8b
Joseph V <joseph.vonedwins@yahoo.com>
2018-10-07 15:13:41 -0400

    improved styling

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { BrowserRouter as Router, Route } from "react-router-dom";
-import './App.css';
 import Home from "./pages/Home";
 import SearchPage from "./pages/SearchPage";
 import SavedPage from "./pages/SavedPage";
